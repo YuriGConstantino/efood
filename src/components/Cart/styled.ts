a5069708baa378de4105176943210f84e9bf2966
Yuri Gabriel Constantino <yuri.gconstantino@gmail.com>
2025-01-15 18:17:03 -0300

    fix

---
+++ src/components/Cart/styled.ts
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+
+export const Container = styled.div`
+  display: none;
+
+  &.showCart {
+    display: block;
+  }
+`
+export const CartContanier = styled.aside`
+  position: fixed;
+  top: 0;
+  right: 0;
+  padding: 32px 8px;
+  height: 100%;
+  max-width: 360px;
+  width: 100%;
+  background-color: ${colors.red};
+  z-index: 1;
+  > p {
+    display: flex;
+    justify-content: space-between;
+    color: ${colors.beige};
+    margin: 40px 0 16px;
+    font-size: 14px;
+    font-weight: 700;
+  }
+
+  > button {
+    width: 100%;
+    background-color: ${colors.beige};
+    border: none;
+    color: ${colors.red};
+    font-size: 14px;
+    font-weight: 700;
+    padding: 4px;
+    cursor: pointer;
+  }
+`
+
+export const ItemCard = styled.li`
+  position: relative;
+  background-color: ${colors.beige};
+  padding: 8px 12px 8px;
+  display: flex;
+  margin-bottom: 16px;
+  img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    margin-right: 8px;
+  }
+  p {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    font-size: 18px;
+    font-weight: bold;
+
+    span {
+      font-size: 14px;
+      font-weight: 400;
+    }
+  }
+`
+export const RemoveBtn = styled.button`
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  img {
+    width: 16px;
+    height: 16px;
+  }
+`
--- src/pages/RestaurantPerfil/index.tsx
+++ src/pages/RestaurantPerfil/index.tsx
@@ -4,21 +4,21 @@ import { Header } from '../../containers/Header'
 import { RestaurentMenu } from '../../containers/RestaurentMenuList'
 import { useGetRestaurantMenuQuery } from '../../services/api'
 
-// export const RestaurantPerfil = () => {
-//   const { id } = useParams()
+export const RestaurantPerfil = () => {
+  const { id } = useParams()
 
-//   const { data: restaurantPerfil } = useGetRestaurantMenuQuery(id!)
+  const { data: restaurantPerfil } = useGetRestaurantMenuQuery(id!)
 
-//   if (restaurantPerfil) {
-//     return (
-//       <>
-//         <Header showRestaurantPerfil={true} />
-//         {restaurantPerfil && <Hero restaurant={restaurantPerfil} />}
-//         {restaurantPerfil && (
-//           <RestaurentMenu cardapio={restaurantPerfil.cardapio} />
-//         )}
-//       </>
-//     )
-//   }
-//   return <h3>Carregando...</h3>
-// }
+  if (restaurantPerfil) {
+    return (
+      <>
+        <Header showRestaurantPerfil={true} />
+        {restaurantPerfil && <Hero restaurant={restaurantPerfil} />}
+        {restaurantPerfil && (
+          <RestaurentMenu cardapio={restaurantPerfil.cardapio} />
+        )}
+      </>
+    )
+  }
+  return <h3>Carregando...</h3>
+}
